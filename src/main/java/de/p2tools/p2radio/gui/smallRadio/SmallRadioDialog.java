73c654e6b5e10d068fc78b0872834527f5b56d7d
W.Xaver <w.xaver@googlemail.com>
2022-12-04 13:27:13 +0100

    smallRadioDialog

--- src/main/java/de/p2tools/p2radio/gui/smallRadio/SmallRadioDialog.java
+++ src/main/java/de/p2tools/p2radio/gui/smallRadio/SmallRadioDialog.java
@@ -19,12 +19,18 @@ package de.p2tools.p2radio.gui.smallRadio;
 import de.p2tools.p2Lib.dialogs.dialog.PDialogOnly;
 import de.p2tools.p2radio.controller.config.ProgConfig;
 import de.p2tools.p2radio.controller.config.ProgData;
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.StageStyle;
 
 
 public class SmallRadioDialog extends PDialogOnly {
     private double xOffset = 0;
     private double yOffset = 0;
+    private double xSize = 0;
+    private double ySize = 0;
+    private double xPos = 0;
+    private double yPos = 0;
 
     SmallRadioDialog() {
         super(ProgData.getInstance().primaryStage, ProgConfig.SMALL_RADIO_SIZE,
@@ -32,16 +38,109 @@ public class SmallRadioDialog extends PDialogOnly {
         init(false);
 
         getStage().getScene().setOnMousePressed(event -> {
-            xOffset = event.getSceneX();
-            yOffset = event.getSceneY();
+            xOffset = event.getSceneX(); //Pos im Fenster von links
+            yOffset = event.getSceneY(); //Pos im Fenster von oben
+            xSize = getStage().getWidth(); //Fensterbreite
+            ySize = getStage().getHeight(); //Fensterhöhe
+            xPos = event.getScreenX(); //Pos vom Bildschirmrand
+            yPos = event.getScreenY(); //Pos vom Bildschirmrand
+
+            if (yOffset < 20 && xOffset < 20) {
+                //oben links
+                getStage().getScene().setCursor(Cursor.NW_RESIZE);
+            } else if (yOffset < 20 && xOffset > xSize - 20) {
+                //oben rechts
+                getStage().getScene().setCursor(Cursor.NE_RESIZE);
+            } else if (yOffset > ySize - 20 && xOffset > xSize - 20) {
+                //unten rechts
+                getStage().getScene().setCursor(Cursor.SE_RESIZE);
+            } else if (yOffset > ySize - 20 && xOffset < 20) {
+                //unten links
+                getStage().getScene().setCursor(Cursor.SW_RESIZE);
+            } else if (yOffset < 20) {
+                //oben
+                getStage().getScene().setCursor(Cursor.N_RESIZE);
+            } else if (yOffset > ySize - 20) {
+                //unten
+                getStage().getScene().setCursor(Cursor.S_RESIZE);
+            } else if (xOffset < 20) {
+                //links
+                getStage().getScene().setCursor(Cursor.E_RESIZE);
+            } else if (xOffset > xSize - 20) {
+                //rechts
+                getStage().getScene().setCursor(Cursor.W_RESIZE);
+            } else {
+                //dann nur schieben
+                getStage().getScene().setCursor(Cursor.OPEN_HAND);
+            }
+        });
+        getStage().getScene().setOnMouseReleased(event -> {
+            getStage().getScene().setCursor(Cursor.DEFAULT);
         });
         getStage().getScene().setOnMouseDragged(event -> {
-            getStage().setX(event.getScreenX() - xOffset);
-            getStage().setY(event.getScreenY() - yOffset);
+            if (yOffset < 20 && xOffset < 20) {
+                //oben links
+                moveUp(event);
+                moveLeft(event);
+            } else if (yOffset < 20 && xOffset > xSize - 20) {
+                //oben rechts
+                moveUp(event);
+                moveRight(event);
+            } else if (yOffset > ySize - 20 && xOffset > xSize - 20) {
+                //unten rechts
+                moveDown(event);
+                moveRight(event);
+            } else if (yOffset > ySize - 20 && xOffset < 20) {
+                //unten links
+                moveDown(event);
+                moveLeft(event);
+            } else if (yOffset < 20) {
+                //oben
+                moveUp(event);
+            } else if (yOffset > ySize - 20) {
+                //unten
+                moveDown(event);
+            } else if (xOffset < 20) {
+                //links
+                moveLeft(event);
+            } else if (xOffset > xSize - 20) {
+                //rechts
+                moveRight(event);
+            } else {
+                //dann nur schieben
+                getStage().setX(event.getScreenX() - xOffset);
+                getStage().setY(event.getScreenY() - yOffset);
+            }
         });
         getStage().initStyle(StageStyle.TRANSPARENT);
         getVBoxCompleteDialog().getStyleClass().add("smallRadio");
 
         super.showDialog();
+    }
+
+    private void moveRight(MouseEvent event) {
+        double newX = xSize + event.getScreenX() - xPos;
+        getStage().setWidth(newX);
+    }
+
+    private void moveLeft(MouseEvent event) {
+        double newX = xSize + xPos - event.getScreenX();
+        double moveX = xPos - event.getScreenX();
+        System.out.println(moveX);
+        getStage().setWidth(newX);
+        getStage().setX(xPos - moveX);
+    }
+
+    private void moveUp(MouseEvent event) {
+        double newY = ySize + yPos - event.getScreenY();
+        double moveY = yPos - event.getScreenY();
+        System.out.println(moveY);
+        getStage().setHeight(newY);
+        getStage().setY(yPos - moveY);
+    }
+
+    private void moveDown(MouseEvent event) {
+        double newY = ySize + event.getScreenY() - yPos;
+        getStage().setHeight(newY);
     }
 }
